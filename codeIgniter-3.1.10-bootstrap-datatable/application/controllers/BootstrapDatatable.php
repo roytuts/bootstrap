e85b8e8f9d4b1e506f46046f8087485622571c8e
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-07-26 13:00:13 +0530

    Add files via upload

---
+++ codeIgniter-3.1.10-bootstrap-datatable/application/controllers/BootstrapDatatable.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* Description of BootstrapDatatable_Model
+*
+* @author https://www.roytuts.com
+*/
+class BootstrapDatatable extends CI_Controller {
+	
+	function __construct() {
+        parent::__construct();
+        $this->load->model('BootStrapDatatable_Model', 'dm');
+    }
+	
+	function index() {
+		$this->load->view('products', NULL);
+	}
+	
+	function get_products() {
+		$products = $this->dm->get_products();
+		echo json_encode($products);
+	}
+	
+	function delete_product() {
+		$id = isset($_POST['id']) ? $_POST['id'] : NULL;
+		
+		if($this->dm->delete_product($id) === TRUE) {
+			return TRUE;
+		}
+		
+		return FALSE;
+	}
+	
+	function update_product() {
+		$id = $_POST['id'];
+		$name = $_POST['name'];
+		$price = $_POST['price'];
+		$sale_price = $_POST['sale_price'];
+		$sale_count = $_POST['sale_count'];
+		$sale_date = $_POST['sale_date'];
+		
+		if($this->dm->update_product($id, $name, $price, $sale_price, $sale_count, $sale_date) === TRUE) {
+			return TRUE;
+		}
+		
+		return FALSE;
+	}
+	
+	function add_product() {
+		$name = $_POST['name'];
+		$price = $_POST['price'];
+		$sale_price = $_POST['sale_price'];
+		$sale_count = $_POST['sale_count'];
+		$sale_date = $_POST['sale_date'];
+		
+		if($this->dm->add_product($name, $price, $sale_price, $sale_count, $sale_date) === TRUE) {
+			return TRUE;
+		}
+		
+		return FALSE;
+	}
+}
+/* End of file Datatable.php */
+/* Location: ./application/controllers/Datatable.php */
---
+++ codeIgniter-3.1.10-bootstrap-datatable/readme.rst
@@ -0,0 +1,1 @@
+You can read tutorial https://www.roytuts.com/bootstrap-datatable-crud-using-codeigniter-mysql-ajax
